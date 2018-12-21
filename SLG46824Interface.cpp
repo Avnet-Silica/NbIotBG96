572dbfc6b980421fe4ae3c102d66a5ecb40c766e
Alex <Alessandro.ratti@avnet.eu>
2018-12-21 15:02:00 +0100

    Header files comments update

--- SLG46824Interface.cpp
+++ SLG46824Interface.cpp
@@ -1,35 +1,9 @@
-/* mbed Microcontroller Library
-* Copyright (c) 20015 ARM Limited
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 /**
   ******************************************************************************
-  * @file    BG96Interface.cpp 
-  * @author  STMicroelectronics
-  * @brief   Implementation of the NetworkStack for the BG96 Device
-  ******************************************************************************
-  * @copy
+  * @file    SLG46824 Interface.cpp 
+  * @author  Avnet Silica
+  * @brief   cpp interface for SLG46824 class
   *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2016 STMicroelectronics</center></h2>
   ******************************************************************************
   */
   
